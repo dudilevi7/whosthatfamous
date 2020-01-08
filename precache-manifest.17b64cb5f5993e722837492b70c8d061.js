ac7ec545a7ac2c19d41786f514d15f999186d7ff
dudilevi7 <dlevis10@gmail.com>
2020-01-08 15:49:59 +0200

    Updates

---
+++ precache-manifest.17b64cb5f5993e722837492b70c8d061.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daf3996ef51a6ccb9bda3d2966c71593",
+    "url": "/whosthatfamous/index.html"
+  },
+  {
+    "revision": "64d9c21817f39613327f",
+    "url": "/whosthatfamous/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "569e48782ddd6b8758d5",
+    "url": "/whosthatfamous/static/css/main.afa37ec1.chunk.css"
+  },
+  {
+    "revision": "64d9c21817f39613327f",
+    "url": "/whosthatfamous/static/js/2.9269f9d3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/whosthatfamous/static/js/2.9269f9d3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "569e48782ddd6b8758d5",
+    "url": "/whosthatfamous/static/js/main.6bbf2de1.chunk.js"
+  },
+  {
+    "revision": "cf7613440b2116e60f78",
+    "url": "/whosthatfamous/static/js/runtime-main.3de60a20.js"
+  },
+  {
+    "revision": "958ec92efed6b0ff95ebf5569f3c1723",
+    "url": "/whosthatfamous/static/media/whos_logo.958ec92e.png"
+  }
+]);
